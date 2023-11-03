658c91491828765842434f939bf16d57b54919b3
gcculberson <g.c.culberson@gmail.com>
2023-11-03 16:03:06 -0600

    added some commits

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -10,8 +10,15 @@ public class ComputerPlayer extends Player{
 	public ComputerPlayer(String name, String color, int row, int col) {
 		super(name, color, row, col);
 	}
+	
+	
 	public Solution createSuggestion(Solution solution) {
-		return null;
+		return solution;
+		
+		/*While player was in the room
+		 * 
+		 */
+		
 		
 	}
 	public BoardCell selectTarget() {
