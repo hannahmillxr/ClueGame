50a39b300fbc87e8ecdfa14284cbd077b5162e51
GillianCulberson <144496129+GillianCulberson@users.noreply.github.com>
2023-11-29 14:36:41 -0700

    acussation

---
+++ src/clueGame/GuessDialog.java
@@ -0,0 +1,33 @@
+package clueGame;
+
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+
+public class GuessDialog extends JDialog {
+	private JComboBox<String> person, room, weapon;
+	
+	public GuessDialog(Board board) {
+		person = new JComboBox<String>();
+		for(Card p: board.getPersonCards()) {
+			person.addItem(p.getCardName());;
+		}
+		add(person);
+		room = new JComboBox<String>();
+		for(Card r: board.getRoomCards()) {
+			room.addItem(r.getCardName());
+		}
+		add(room);
+		
+		weapon = new JComboBox<String>();
+		for(Card w: board.getWeaponCards()) {
+			person.addItem(w.getCardName());
+		}
+		add(weapon);
+		
+		
+	}
+	
+	
+	
+
+}
